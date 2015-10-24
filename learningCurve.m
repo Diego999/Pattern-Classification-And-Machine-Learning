488b233e401c9d61d3fdd4a354dbfb7948657364
Diego Antognini <diegoantognini@gmail.com>
2015-10-24 17:28:11 +0200

    Fix bug RMSE

--- learningCurve.m
+++ learningCurve.m
@@ -10,28 +10,30 @@ y = y_train;
 X = X_train;
 N = length(y);
 
-[y_cls1, X_cls1, y_cls2, X_cls2, y_cls3, X_cls3] = preprocess(y, X);
+numberOfExperiments = 2;
+proportionOfTraining = 0.8;
+
 d1 = 3;
 d2 = 5;
 d3 = 2;
 
-l1 = 0.035;
-l2 = 0.1298;
-l3 = 0.7480;
-
-k1 = 5;
-k2 = 5;
+k1 = 10;
+k2 = 6;
 k3 = 4;
 
-K = 0:5:95;
+K = 10:10:90;
 K = K(2:end);
 K = K./100;
-for i = 1:1:length(K)
-    k = K(i);
-    
-    [err_tr_cls1(:,i), err_te_cls1(:,i)] = modelLearningCurve(k, k1, d1, l1, y_cls1, X_cls1);
-    [err_tr_cls2(:,i), err_te_cls2(:,i)] = modelLearningCurve(k, k2, d2, l2, y_cls2, X_cls2);
-    [err_tr_cls3(:,i), err_te_cls3(:,i)] = modelLearningCurve(k, k3, d3, l3, y_cls3, X_cls3);
+
+for j = 1:1:numberOfExperiments
+    setSeed(28111993*j);
+    [XTr, yTr, XTe, yTe] = splitProp(proportionOfTraining, y, X);
+    fprintf('%d\n', j); 
+
+    for i = 1:1:length(K)
+        k = K(i);
+        [err_tr_cls1(j,i), err_te_cls1(j,i), err_tr_cls2(j,i), err_te_cls2(j,i), err_tr_cls3(j,i), err_te_cls3(j,i)] = modelLearningCurve(k, k1, k2, k3, yTr, XTr, d1, d2, d3, yTe, XTe);
+    end
 end
 
 figure
--- modelLearningCurve.m
+++ modelLearningCurve.m
@@ -1,55 +1,54 @@
 % Written by Diego Antognini & Jason Racine, EPFL 2015
 % all rights reserved
 
-function [err_tr, err_te_cv] = modelLearningCurve(prop, K, d, lambda, y, X)
-    for i = 1:1:10
-        setSeed(28111993*i);
-        [XTr, yTr, XTe, yTe] = splitProp(prop, y, X);
+function [err_tr_cls1, err_te_cls1, err_tr_cls2, err_te_cls2, err_tr_cls3, err_te_cls3] = modelLearningCurve(prop, k1, k2, k3, y, X, d1, d2, d3, globalYTe, globalXTe)
+    [XTr, yTr, XTe, yTe] = splitProp(prop, y, X);
+    
+     lambda_cls1 = findLambda(k1, yTr, XTr, 1, d1, d2, d3);
+     lambda_cls2 = findLambda(k2, yTr, XTr, 2, d1, d2, d3);
+     lambda_cls3 = findLambda(k3, yTr, XTr, 3, d1, d2, d3);
+    
+     [y_cls1, X_cls1, y_cls2, X_cls2, y_cls3, X_cls3, idx_cls1, idx_cls2, idx_cls3] = preprocess(yTr, XTr, d1, d2, d3);
 
-        % CV Error
-        N = length(yTr);
-        idxCV = splitGetCV(K, N);
-        for k=1:1:K
-            [XXTr, yyTr, XXTe, yyTe] = splitGetTrTe(yTr, XTr, idxCV, k);
-
-            tXTr = [ones(length(yyTr),1) buildPoly(XXTr, d)];
-            tXTe = [ones(length(yyTe),1) buildPoly(XXTe, d)];
-
-            beta_rr = ridgeRegression(yyTr, tXTr, lambda);
-            err_te_rr_cv(k) = RMSE(yyTe, tXTe*beta_rr);
-        end
-        err_te_cv(i) = mean(err_te_rr_cv);
-        
-        % Training Error
-        tXTr = [ones(length(yTr),1) buildPoly(XTr, d)];
-        beta_rr = ridgeRegression(yTr, tXTr, lambda);
-        err_tr(i) = RMSE(yTr, tXTr*beta_rr);
-    end
+     tX_cls1 = [ones(length(y_cls1),1) X_cls1];
+     tX_cls2 = [ones(length(y_cls2),1) X_cls2];
+     tX_cls3 = [ones(length(y_cls3),1) X_cls3];
+    
+     beta_cls1 = ridgeRegression(y_cls1, tX_cls1, lambda_cls1);
+     beta_cls2 = ridgeRegression(y_cls2, tX_cls2, lambda_cls2);
+     beta_cls3 = ridgeRegression(y_cls3, tX_cls3, lambda_cls3);
+     
+     err_tr_cls1 = RMSE(y_cls1, tX_cls1*beta_cls1);
+     err_tr_cls2 = RMSE(y_cls2, tX_cls2*beta_cls2);
+     err_tr_cls3 = RMSE(y_cls3, tX_cls3*beta_cls3);
+     
+     [y_cls1, X_cls1, y_cls2, X_cls2, y_cls3, X_cls3, idx_cls1, idx_cls2, idx_cls3] = preprocess(globalYTe, globalXTe, d1, d2, d3);
+ 
+     tXTe_cls1 = [ones(length(y_cls1),1) X_cls1];
+     tXTe_cls2 = [ones(length(y_cls2),1) X_cls2];
+     tXTe_cls3 = [ones(length(y_cls3),1) X_cls3];
+     
+     err_te_cls1 = RMSE(y_cls1, tXTe_cls1*beta_cls1);
+     err_te_cls2 = RMSE(y_cls2, tXTe_cls2*beta_cls2);
+     err_te_cls3 = RMSE(y_cls3, tXTe_cls3*beta_cls3);
 end
 
-% % % Written by Diego Antognini & Jason Racine, EPFL 2015
-% % % all rights reserved
-% % 
+
 % function [err_tr, err_te_cv] = modelLearningCurve(prop, K, d, lambda, y, X)
-%     % Fix the test size
-%     setSeed(1111);
-%     [X_, y_, X_test, y_test] = splitProp(0.8, y, X);
-%     
 %     for i = 1:1:10
 %         setSeed(28111993*i);
-%         [XTr, yTr, XTe, yTe] = splitProp(prop, y_, X_);
+%         
 % 
 %         % CV Error
-%         N = length(yTr);
-%         idxCV = splitGetCV(K, N);
+%         
 %         for k=1:1:K
 %             [XXTr, yyTr, XXTe, yyTe] = splitGetTrTe(yTr, XTr, idxCV, k);
 % 
-%             tXTr = [ones(length(yyTr),1) XXTr];
-%             tXTe = [ones(length(y_test),1) X_test];
+%             tXTr = [ones(length(yyTr),1) buildPoly(XXTr, d)];
+%             tXTe = [ones(length(yyTe),1) buildPoly(XXTe, d)];
 % 
 %             beta_rr = ridgeRegression(yyTr, tXTr, lambda);
-%             err_te_rr_cv(k) = RMSE(y_test, tXTe*beta_rr);
+%             err_te_rr_cv(k) = RMSE(yyTe, tXTe*beta_rr);
 %         end
 %         err_te_cv(i) = mean(err_te_rr_cv);
 %         
@@ -59,4 +58,5 @@ end
 %         err_tr(i) = RMSE(yTr, tXTr*beta_rr);
 %     end
 % end
-
+% 
+% 
